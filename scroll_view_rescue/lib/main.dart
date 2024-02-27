8bd18e11e2e581586dbb6d41a6a9b16c14feb3ac
Yasin Ertekin <yasinertekin24@gmail.com>
2024-02-27 12:22:34 +0300

    Add ScrollViewRescue widget and implement end of scroll view alert

--- scroll_view_rescue/lib/main.dart
+++ scroll_view_rescue/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:scroll_view_rescue/scroll_veiw_rescue.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,7 +32,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: ScrollViewRescue(),
     );
   }
 }
--- scroll_view_rescue/lib/scroll_veiw_rescue.dart
+++ scroll_view_rescue/lib/scroll_veiw_rescue.dart
@@ -1,19 +1,63 @@
 import 'package:flutter/material.dart';
 
 class ScrollViewRescue extends StatelessWidget {
-  const ScrollViewRescue({super.key});
+  ScrollViewRescue({super.key});
+
+  final List<String> labels = List.generate(8, (index) => 'Label ${index + 1}');
+  final ScrollController _scrollController = ScrollController();
 
   @override
   Widget build(BuildContext context) {
+    final List<String> labels =
+        List.generate(8, (index) => 'Label ${index + 1}');
+
+    void showEndOfScrollViewAlert(BuildContext context) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('End of Scroll View'),
+            content: const Text('You have reached the end of the scroll view.'),
+            actions: <Widget>[
+              GestureDetector(
+                onTap: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    }
+
     return Scaffold(
       body: SafeArea(
-        child: ListView.builder(
-          itemCount: 8,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text('Label ${index + 1}'),
-            );
+        child: NotificationListener<ScrollEndNotification>(
+          onNotification: (notification) {
+            if (_scrollController.position.extentAfter == 0) {
+              showEndOfScrollViewAlert(context);
+            }
+            return true;
           },
+          child: ListView.builder(
+            controller: _scrollController,
+            itemCount: labels.length,
+            physics: const ClampingScrollPhysics(),
+            itemBuilder: (context, index) {
+              if (index < 3) {
+                return ListTile(
+                  title: Text(labels[index]),
+                );
+              } else {
+                return SizedBox(
+                    height: MediaQuery.of(context).size.height * 2,
+                    child: ListTile(
+                      title: Text(labels[index]),
+                    ));
+              }
+            },
+          ),
         ),
       ),
     );
