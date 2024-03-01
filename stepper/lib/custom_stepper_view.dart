07a54aa4d4d5b6278d83577f94e7e363ae386774
Yasin Ertekin <yasinertekin24@gmail.com>
2024-03-01 11:54:29 +0300

    Refactor custom stepper view and add custom stepper widget

--- stepper/lib/custom_stepper_view.dart
+++ stepper/lib/custom_stepper_view.dart
@@ -14,27 +14,70 @@ final class CustomStepperView extends StatelessWidget with CustomStepperMixin {
         appBar: _CustomStepperViewAppBar(counterViewModel: counterViewModel),
         body: Column(
           children: [
-            Expanded(
-              child: ListView.builder(
-                itemCount: stepList.length,
-                itemBuilder: (BuildContext context, int index) {
-                  final step = stepList[index];
-                  return Padding(
-                    padding: const EdgeInsets.all(8),
-                    child: ListTile(
-                      tileColor: getTileColor(index),
-                      title: Text(step.title),
-                      subtitle: Text(step.content),
-                      leading: Icon(step.icon),
-                      onTap: () {},
-                    ),
-                  );
-                },
-              ),
+            _CustomStepper(
+              counterViewModel: counterViewModel,
+              stepList: stepList,
             ),
             _Buttons(counterViewModel: counterViewModel),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class _CustomStepper extends StatelessWidget {
+  const _CustomStepper({
+    required this.counterViewModel,
+    required this.stepList,
+    super.key,
+  });
+
+  final List<StepList> stepList;
+  final CounterViewModel counterViewModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Stepper(
+        onStepTapped: (step) {
+          counterViewModel.getIncrementValue();
+        },
+        currentStep: counterViewModel._counter ~/ 5,
+        onStepContinue: counterViewModel.increment,
+        onStepCancel: counterViewModel.decrement,
+        controlsBuilder:
+            (BuildContext context, ControlsDetails controlsDetails) {
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              GestureDetector(
+                onTap: controlsDetails.onStepContinue,
+                child: Image.network(
+                  'https://picsum.photos/id/237/200/300',
+                  width: 100,
+                  height: 100,
+                ),
+              ),
+              GestureDetector(
+                onTap: controlsDetails.onStepCancel,
+                child: Image.network(
+                  'https://picsum.photos/id/238/200/300',
+                  width: 100,
+                  height: 100,
+                ),
+              ),
+            ],
+          );
+        },
+        steps: stepList
+            .map(
+              (step) => Step(
+                title: Text(step.title),
+                content: Text(step.content),
+              ),
+            )
+            .toList(),
       ),
     );
   }
@@ -76,12 +119,10 @@ final class _CustomStepperViewAppBar extends StatelessWidget
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      actions: [
-        Text(
-          'Counter: ${counterViewModel.counter}',
-          style: const TextStyle(fontSize: 20),
-        ),
-      ],
+      title: Text(
+        'Counter: ${counterViewModel.counter}',
+        style: const TextStyle(fontSize: 20),
+      ),
     );
   }
 
@@ -210,5 +251,45 @@ final class CounterViewModel extends ChangeNotifier {
 
   int getIncrementValue() {
     return _counter;
+  }
+}
+
+class CustomListViewBuilder extends StatelessWidget {
+  const CustomListViewBuilder({
+    required this.stepList,
+    required this.counterViewModel,
+    super.key,
+  });
+
+  final List<StepList> stepList;
+  final CounterViewModel counterViewModel;
+
+  @override
+  Widget build(BuildContext context) {
+    Color getTileColor(int index) {
+      final incrementValue = counterViewModel.getIncrementValue();
+      final colorIndex =
+          incrementValue ~/ 5; // Divide by 5 to determine color index
+      return index == colorIndex ? Colors.orange : Colors.white;
+    }
+
+    return Expanded(
+      child: ListView.builder(
+        itemCount: stepList.length,
+        itemBuilder: (BuildContext context, int index) {
+          final step = stepList[index];
+          return Padding(
+            padding: const EdgeInsets.all(8),
+            child: ListTile(
+              tileColor: getTileColor(index),
+              title: Text(step.title),
+              subtitle: Text(step.content),
+              leading: Icon(step.icon),
+              onTap: () {},
+            ),
+          );
+        },
+      ),
+    );
   }
 }
